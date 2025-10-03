37beaf9d0241dcba473b11b77f78a0814fe90118
Shubhro Shekhar <shubhroshekhar@gmail.com>
2025-10-03 20:35:19 +0530

    chore: Migrate ESLint configuration to .eslintrc.cjs for module compatibility
    - Created a new .eslintrc.cjs file with ESLint configuration.
    - Removed the old .eslintrc.js file and added a comment indicating the migration for compatibility with type: module.

---
+++ js/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+};
+
+
--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,17 +1,2 @@
-module.exports = {
-  env: {
-    es2022: true,
-    node: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'no-unused-vars': 'error',
-    'no-console': 'off',
-    'prefer-const': 'error',
-    'no-var': 'error',
-  },
-};
+// migrated to .eslintrc.cjs for compatibility with type: module
+module.exports = {};
